3ca4f920f809e913f541c5c564a08b422edc0215
Victor Mateus <victormateus897@gmail.com>
2025-04-24 22:31:42 -0300

    fix: sidebar

--- src/components/layout/dashboard/Sidebar.vue
+++ src/components/layout/dashboard/Sidebar.vue
@@ -1,16 +1,16 @@
 <template>
-  <aside className="w-[19%] max-md:w-full max-md:ml-0">
-    <div className="border-r-[color:var(--Borda,#222A3F)] w-full whitespace-nowrap border-r border-solid">
+  <aside className="min-w-[272px] max-md:w-full max-md:ml-0">
+    <div className="border-r-[color:var(--Borda,#222A3F)] w-full h-full whitespace-nowrap border-r border-solid">
       <div
-        className="self-stretch  w-full gap-2.5 overflow-hidden text-2xl text-[rgba(185,89,25,1)] font-bold text-center leading-none p-6 max-md:px-5">
+        className="self-stretch justify-center items-center h-[80px] w-full gap-2.5 overflow-hidden text-2xl text-[rgba(185,89,25,1)] font-bold text-center leading-none p-6 max-md:px-5">
         TRADYEX
       </div>
-      <nav className=" w-full pt-10 pb-[580px] px-6 max-md:pb-[100px] max-md:px-5">
-        <div className="flex w-full flex-col items-stretch">
-          <div className="gap-2.5 text-sm text-[#8A99AF] font-medium uppercase leading-none pl-[15px]">
-            Menu
-          </div>
-          <div class="self-stretch flex flex-col items-start justify-start gap-2.5">
+      <nav className="w-full pt-10 px-6 max-md:pb-[100px] max-md:px-5">
+        <div className="flex gap-2 w-full flex-col items-stretch">
+          <p class="text-sm text-[#8A99AF] font-medium pl-[15px]">
+            MENU
+          </p>
+          <div class="self-stretch flex flex-col justify-start gap-6">
             <div v-for="(item, index) in menuItems" :key="index">
               <router-link :to="item.route" custom v-slot="{ navigate }">
                 <li @click="navigate" :class="{
@@ -19,7 +19,6 @@
                   'flex w-full items-center': true,
                   'justify-between': isCurrentRoute(item.route),
                   'gap-2.5': !isCurrentRoute(item.route),
-                  [`mt-${index === 0 ? '0' : '6'}`]: true,
                   'pl-4 pr-3 py-2 rounded-[3px]': true,
                   'cursor-pointer': true
                 }">
