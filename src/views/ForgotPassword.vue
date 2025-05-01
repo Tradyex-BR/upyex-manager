a342ec03af5b4d9999df1e5176eabd03c993b17c
Victor Mateus <victormateus897@gmail.com>
2025-05-01 06:00:37 -0300

    feat: static neon png

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     class="max-w-none flex flex-row w-full h-screen bg-[#010309] mx-auto max-md:max-w-[991px] max-md:flex-col max-sm:max-w-screen-sm">
-    <div
-      class="z-[100] flex flex-col justify-center w-6/12 backdrop-blur-lg items-start gap-8 bg-white p-28 left-[section] max-md:w-full">
+    <div class="z-[100] flex flex-col justify-center w-6/12 backdrop-blur-lg items-start gap-8 bg-white bg-[url('/png/static-neon.png')] bg-no-repeat bg-top bg-center p-28 left-[section] max-md:w-full">
+
       <div class="flex flex-col gap-2">
         <h1 class="text-[#040D25] text-[32px] leading-[40px] font-semibold">
           Esqueceu a senha
--- src/views/PasswordChanged.vue
+++ src/views/PasswordChanged.vue
@@ -1,8 +1,9 @@
 <template>
   <div
     class="max-w-none flex flex-row w-full h-screen bg-[#010309] mx-auto max-md:max-w-[991px] max-md:flex-col max-sm:max-w-screen-sm">
-    <div class="flex z-50 flex-col justify-center w-6/12 backdrop-blur-lg items-center gap-8 bg-white p-28 left-[section] max-md:w-full">
-      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <div class="z-[100] flex flex-col justify-center w-6/12 backdrop-blur-lg items-start gap-8 bg-white bg-[url('/png/static-neon.png')] bg-no-repeat bg-top bg-center p-28 left-[section] max-md:w-full">
+    
+    <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" class="self-center">
         <path
 d="M52.1627 19.8508L32.0134 40.0002L21.9414 29.9255" stroke="#01DD56" stroke-width="4"
           stroke-linecap="round" stroke-linejoin="round" />
@@ -11,7 +12,7 @@ d="M52.1627 19.8508L32.0134 40.0002L21.9414 29.9255" stroke="#01DD56" stroke-wid
           stroke="#01DD56" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
       </svg>
 
-      <div class="flex flex-col gap-2 items-center">
+      <div class="flex flex-col gap-2 items-center self-center">
       <h1 class="text-[#040D25] text-[32px] leading-[40px] font-semibold">
         Senha alterada com sucesso
       </h1>
