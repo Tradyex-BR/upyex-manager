f9be3ade149ccd86307bdf3461f2b191bf041855
Victor Mateus <victormateus897@gmail.com>
2025-05-05 01:48:09 -0300

    chore: copy button visual feedback

--- src/components/common/CopyButton.vue
+++ src/components/common/CopyButton.vue
@@ -1,19 +1,30 @@
 <template>
-  <button @click="handleCopy" class="p-0 transition-colors bg-transparent border-none outline-none"
+  <button @click="handleCopy" class="p-0 transition-colors bg-transparent border-none outline-none focus:outline-none focus:ring-0 ring-0 w-[25px] h-[25px] flex items-center justify-center"
     title="Copiar ID da Transação">
-    <CopyIcon />
+    <CopyIcon v-if="!copied" class="w-[25px] h-[25px]" />
+    <svg v-else xmlns="http://www.w3.org/2000/svg" class="w-[25px] h-[25px] text-green-500" viewBox="0 0 20 20" fill="currentColor">
+      <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+    </svg>
   </button>
 </template>
 <script setup lang="ts">
 import CopyIcon from '@/components/icons/CopyIcon.vue'
 import { useToast } from 'vue-toastification'
+import { ref } from 'vue'
 
 const toast = useToast()
+const copied = ref(false)
 
 const handleCopy = async () => {
   try {
     await navigator.clipboard.writeText(props.stringToCopy)
+    copied.value = true
     toast.success('ID copiado com sucesso!')
+    
+    // Resetar o estado após 2 segundos
+    setTimeout(() => {
+      copied.value = false
+    }, 2000)
   } catch (err) {
     toast.error('Erro ao copiar ID')
     console.error('Erro ao copiar para a área de transferência:', err)
